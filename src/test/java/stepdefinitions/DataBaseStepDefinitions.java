76bc06a5251c59b0fa13eabfaa1af9be90bf297a
hayriye uylukcu <huylukcu@gmail.comgit config --global user.email huylukcu@gmail.comgit config --global user.name hayriye>
2023-02-15 11:43:37 -0600

    commit

--- src/test/java/stepdefinitions/DataBaseStepDefinitions.java
+++ src/test/java/stepdefinitions/DataBaseStepDefinitions.java
@@ -1,10 +1,12 @@
 package stepdefinitions;
 
 import io.cucumber.java.en.Given;
+import utilities.DBUtils;
 
 public class DataBaseStepDefinitions {
     @Given("user connects to the application database")
     public void user_connects_to_the_application_database() {
-
+//        creating database connections using database utility class
+        DBUtils.createConnection();
     }
 }
